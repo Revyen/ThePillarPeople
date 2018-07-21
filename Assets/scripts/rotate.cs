05c8bf083eba4296b0f40822ce2428cb2c6e8bea
Unknown <denise.schwarze016@stud.fh-dortmund.de>
2018-07-21 18:10:41 +0200

    Added rotate-script for cookie model and missing files

---
+++ Assets/scripts/rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    public float rotationalSpeed;
+    public float movementSpeed;
+    public float range;
+
+	// Use this for initialization
+	void Start () {
+        rotationalSpeed = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        float newY = Mathf.Sin(Time.time * movementSpeed);
+        transform.position = new Vector3(pos.x, newY * range, pos.z);
+        transform.Rotate(Vector3.forward * rotationalSpeed * Time.deltaTime);
+	}
+
+
+    //transform.Translate(new Vector3(0, 1, 0) * Mathf.Sin(1) * rotationalSpeed * Time.deltaTime,Space.World);
+}
