d168c59a7ba58463d5fccea1c0f022a5627ba15e
Dimitri <dimitrisimonow90@googlemail.com>
2018-07-22 02:07:51 +0200

    Upgrade Button Function Added

---
+++ Assets/scripts/clickbutton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class clickbutton : MonoBehaviour {
+    rotate mainCookie;
+
+    // Use this for initialization
+    void Start () {
+        mainCookie = GameObject.FindGameObjectWithTag("mainCookie").GetComponent<rotate>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        mainCookie.changeCounter(-5);
+        Debug.Log("Change!");
+    }
+}
